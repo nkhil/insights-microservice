686d1f4d8b20fd5be0aeb0f23f1fe45c7fff3f6d
dingers@jigsaw.xyz <Zebedee@Daves-MBP-2.connect>
2018-07-05 02:23:17 +0100

    fixing up

--- src/routers/clients.js
+++ src/routers/clients.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const logger = require('../logger');
-const { clientsController } = require('../controllers');
+const clientsController = require('../controllers/clients');
 const {
   DuplicateError,
   InvalidParametersError,
