a7bd0879656e8877e59a827fdb30d79bb3ba2dd7
Tom Stuart <thomascstuart@gmail.com>
2018-07-16 17:29:44 +0100

    Handle Non JSON

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ const healthCheckRouter = require('./routers/healthcheck');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(middlewares.parseExceptionCatcher());
 app.use(middlewares.trackingInit());
 app.use(middlewares.requestInit());
 app.use(middlewares.schemaValidator(`${__dirname}/../definitions/getting-started.yaml`));
 
-app.use('/', healthCheckRouter);
-app.use('/clients', clientRouter);
+app.use('/gettingstarted', healthCheckRouter);
+app.use('/gettingstarted/clients', clientRouter);
 
 app.use(middlewares.defaultErrorHandler());
 app.use(middlewares.logsClose());
