1287ef774f45a25cddf13f937f579dbb38541038
Matt Camp <matt@noise.net.nz>
2018-07-23 16:32:05 +0100

    debug coverage

--- tests/component.sh
+++ tests/component.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 set -e
-
-LOG_LEVEL=INFO ./node_modules/.bin/nyc --clean=false --reporter=lcov --report-dir ./coverage/component node index.js &
+export LOG_LEVEL=INFO
+./node_modules/.bin/nyc --clean=false --reporter=lcov --report-dir ./coverage/component node index.js &
 SERVER_PID=$!;
+ps -a
 sleep 5
 npm run test:feature
 kill $SERVER_PID
