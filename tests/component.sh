2f273326b588ce6977696a845ce07a37511a7225
Matt Camp <matt@noise.net.nz>
2018-07-23 16:46:06 +0100

    test

--- tests/component.sh
+++ tests/component.sh
@@ -5,6 +5,7 @@ export LOG_LEVEL=INFO
 ./node_modules/.bin/nyc --clean=false --reporter=lcov --report-dir ./coverage/component node index.js &
 SERVER_PID=$!;
 echo "SERVER_PID: ${SERVER_PID}"
+ps -ax
 sleep 5
 npm run test:feature
 kill $SERVER_PID
