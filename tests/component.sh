ba4bd2238f1f793689b72493d1c1d20dc4e88f59
Matt Camp <matt@noise.net.nz>
2018-07-31 15:04:40 +0100

    fix feature test error check

--- tests/component.sh
+++ tests/component.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 #set -e
+
+checkerror() {
+  RC=$?
+  if [ $RC -ne 0 ]; then
+    echo -e "ERROR: $*"
+    exit 1
+  fi
+}
+
 export LOG_LEVEL=INFO
 ./node_modules/.bin/nyc --clean=false --reporter=lcov --report-dir ./coverage/component node index.js &
 SERVER_PID=$!;
 echo "SERVER_PID: ${SERVER_PID}"
 sleep 5
 npm run test:feature
+checkerror "Failed feature tests"
 kill $SERVER_PID
 sleep 5
 ./node_modules/.bin/nyc report --reporter=text-summary
